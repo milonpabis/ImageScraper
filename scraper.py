d824287d79330323793736cd17eaab60f21bd80a
milonpabis <miloszpabis@onet.pl>
2024-05-20 14:29:24 +0200

    Improved, working scraper1

--- scraper.py
+++ scraper.py
@@ -2,17 +2,25 @@ import os
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import base64
 import time
 import requests
+from datetime import datetime
 
 COOKIE_ACCEPT = '/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button'
 MORE_BUTTON = '//*[@id="islmp"]/div/div/div[1]/div/div[2]/span'
 SCROLLS = 0
 DIR = 'product'
 
+COOKIES_XPATH = "/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button/span"
+COOKIES_XPATH2 = "/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button/div[3]"
+COOKIES_TEXT = "//span[text()='Accept all'] | //div[text()='Accept all']"
 
+
+MORE_BUTTON_1 = "//span[text()='Accept all'] | //div[text()='Accept all']"
 
 
 class Scraper:
@@ -20,7 +28,9 @@ class Scraper:
     def __init__(self, image_name, directory=DIR):
 
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
+        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_argument("--lang=en-GB")
 
         self.dir = directory
         self.name = image_name
@@ -29,40 +39,36 @@ class Scraper:
         self.path = f'{self.dir}\\{self.name}'
         print(self.path)
 
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.execute_and_encode()
 
 
     def accept_cookies(self):
-        time.sleep(1)
         try:
-            cook = self.driver.find_element(By.XPATH, COOKIE_ACCEPT)
-            cook.click()
-        except NoSuchElementException:
-            return
+            cookies = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, COOKIES_TEXT)))
+            cookies.click()
+            
+        except ElementClickInterceptedException:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            cookies.click()
+        except TimeoutException as e:
+            print("COOKIES:", e)
+            try:
+                cookies = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, COOKIES_XPATH2)))
+                cookies.click()
+            except Exception as e:
+                print("COOKIES:", e)
+                try:
+                    cookies = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.XPATH, COOKIES_XPATH)))
+                    cookies.click()
+                except Exception as e:
+                    print("COOKIES:", e)
+                    time.sleep(30)
 
 
     def scroll_down(self):
-        time.sleep(2)
-        boolean = False
-        try:
-            more = self.driver.find_element(By.XPATH, MORE_BUTTON)
-            time.sleep(1)
-            more.click()
-        except:
-            boolean = True
-
-        if boolean:
-            try:
-                more = self.driver.find_element(By.CLASS_NAME, 'LZ4I')
-                time.sleep(1)
-                more.click()
-            except:
-                print('scroll succesful')
-                pass
-
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
+        
 
 
     def execute_and_encode(self):
@@ -75,22 +81,26 @@ class Scraper:
         self.accept_cookies()
 
         print("SCROLLS:", SCROLLS)
-        for _ in range(SCROLLS):
-            self.scroll_down()
-            time.sleep(1)
+        start = datetime.now()
 
-        time.sleep(3)
-        images = self.driver.find_elements(By.CLASS_NAME, 'rg_i')
+        while True:
+            self.scroll_down()
+            if (datetime.now() - start).total_seconds() > 10:
+                break
+            
+
+        time.sleep(1)
+        images = self.driver.find_elements(By.TAG_NAME, 'img')
         print(len(images))
 
         counter = 0
         for im in images:
             extension = ''
             img_src = im.get_attribute('src')
-            print(img_src)
-
-            if img_src is not None:
-
+            height = im.get_attribute('height')
+            
+            if img_src is not None and "FAVICON" not in img_src and "google" not in img_src and int(height) > 60:
+                print(img_src)
                 if img_src.startswith('data'):
                     extension = img_src[11:15]
                     if extension.endswith(';'):
@@ -100,11 +110,11 @@ class Scraper:
                 to_decode = img_src.split(f'data:image/{extension};base64,')
                 im_path = self.path + f'\\{self.name}{counter}'
                 if len(to_decode) > 1:
-                    with open(im_path + f'.{extension}', 'wb') as file:
+                    with open(im_path + f'.jpg', 'wb') as file:
                         file.write(base64.b64decode(to_decode[1]))
                 else:
                     respond = requests.get(img_src)
-                    with open(im_path + '.jpeg', 'wb') as file:
+                    with open(im_path + '.jpg', 'wb') as file:
                         file.write(respond.content)
                 counter += 1
         time.sleep(3)
--- src/scraperv2.py
+++ src/scraperv2.py
@@ -3,10 +3,9 @@ from time import sleep
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import random as rd
+import datetime as dt
 
 ENDPOINT = "https://www.pexels.com/search/"
 COOKIES_TEXT = "//span[text()='Accept All Cookies'] | //div[text()='Accept All Cookies'] | //button[text()='Accept All Cookies']"
@@ -18,11 +17,25 @@ class ScraperV2:
 
     
     def run(self, word: str, num: int, hq: bool = True) -> None:
+        if not os.path.exists(self.output):
+            os.makedirs(self.output, exist_ok=True)
+
         urls = self._get_urls(word, num, hq=hq)
         self._download_images(urls)
 
     
     def _get_urls(self, word: str, num: int, hq: bool = True) -> list[str]:
+        """
+        Scrapes the urls from the website.
+
+        Args:
+        - word: the word to be searched.
+        - num: number of images to be downloaded.
+        - hq: if True, returns only high quality images urls.
+
+        Returns:
+        - list of filtered, scraped urls.
+        """
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--lang=en-GB")
         driver = webdriver.Chrome(chrome_options)
@@ -35,6 +48,7 @@ class ScraperV2:
 
         images = driver.find_elements(By.TAG_NAME, "img")
         while len(images) < num:
+            print(len(images))
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             sleep(1)
             images = driver.find_elements(By.TAG_NAME, "img")
@@ -46,6 +60,14 @@ class ScraperV2:
     
 
     def _download_images(self, urls: list[str]) -> None:
+        """
+        Downloads the images from the urls.
+
+        Args:
+        - urls: list of urls to be downloaded.
+
+        Returns: None
+        """
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
         
         for idx, url in enumerate(urls):
@@ -63,13 +85,30 @@ class ScraperV2:
 
 
     def _filter_urls(self, urls: list[str], hq: bool = True) -> list[str]:
+        """
+        Filters the urls to skip icons and other non-image urls.
+
+        Args:
+        - urls: list of urls to be filtered.
+        - hq: if True, returns only high quality images urls.
+
+        Returns:
+        - list of filtered urls.
+        """
         urls = list(filter(lambda u: "/photos/" in u, urls))
         if hq:  
             return list(map(lambda u: u.split("?auto")[0], urls))
         return urls
+    
+
+    def set_output(self, path: str) -> None:
+        self.output = path
         
         
 
-
-scr = ScraperV2("F:/Desktop/TEST_LOW/")
-scr.run("cat", 100, hq=False)
+start = dt.datetime.now()
+scr = ScraperV2("F:/Desktop/images/fat_w/")
+scr.run("obese woman", 1000, hq=False)
+scr.set_output("F:/Desktop/images/fat_m/")
+scr.run("obese man", 1000, hq=False)
+print("EXECUTION TIME:", dt.datetime.now() - start)
